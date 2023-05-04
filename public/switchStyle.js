f60437fa41fc2a31a1d527b9ab55b587a8a25e00
ArtInLines <linesinart@gmail.com>
2023-05-04 16:09:20 +0200

    Template for simpler switchStyle

--- public/switchStyle.js
+++ public/switchStyle.js
@@ -1,7 +1,3 @@
 function switchStyle(styleName) {
-	var styleLink = document.getElementById('style-link');
-	var lastStyleSheet = styleLink.getAttribute('href');
-	if (lastStyleSheet != styleName) {
-		styleLink.setAttribute('href', '/public/' + styleName);
-	}
+	document.body.className = styleName;
 }
