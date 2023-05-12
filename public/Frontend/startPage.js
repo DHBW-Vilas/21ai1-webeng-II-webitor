c55d48dfa68d90b1b64acd78641472823f44ad14
ArtInLines <linesinart@gmail.com>
2023-05-12 08:26:12 +0200

    Unified StartPage js scripts

--- public/Frontend/startPage.js
+++ public/Frontend/startPage.js
@@ -1,3 +1,24 @@
+const workspaceParentDiv = document.getElementById('workspaces');
+
+fetch('/workspaces')
+	.then((res) => res.json())
+	.then((res) => {
+		if (res.success) {
+			res.workspaces.forEach((workspace) => {
+				const workspaceEl = document.createElement('div');
+				workspaceEl.innerText = workspace.name;
+				workspaceEl.setAttribute('data-id', workspace._id);
+				workspaceEl.addEventListener('click', (ev) => openWorkspace(ev.target.getAttribute('data-id')));
+				workspaceParentDiv.insertAdjacentElement('beforeend', workspaceEl);
+			});
+		}
+	});
+
+function openWorkspace(workspaceId) {
+	localStorage.setItem('workspaceId', workspaceId);
+	window.location.href = '/editor';
+}
+
 const fileInput = document.getElementById('fileInput');
 const nameInput = document.getElementById('name');
 
