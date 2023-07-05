9ba3968a7cda93adf3f07669b8fbfcb7ffa6bac3
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2023-07-05 15:14:36 +0200

    fix: light-mode as default + unified common styles

--- public/switchStyle.js
+++ public/switchStyle.js
@@ -2,7 +2,7 @@ let styleMode = localStorage.getItem('style');
 if (styleMode) switchStyle(styleMode);
 
 function switchStyle(mode) {
-	document.body.classList.remove('default', 'light', 'contrast', 'spooky');
+	document.body.classList.remove('light', 'dark', 'contrast', 'spooky');
 	document.body.classList.add(mode);
 	localStorage.setItem('style', mode);
 }
